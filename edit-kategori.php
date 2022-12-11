cac346084f7aa2018388f12eb191bee9b10bdf99
Delmian <108231713+SEKelompok8@users.noreply.github.com>
2022-12-11 19:25:09 +0700

    Update edit-kategori.php

--- edit-kategori.php
+++ edit-kategori.php
@@ -29,10 +29,7 @@
 				<ul>
 			
 					<li><a href="dashboard.php">Dashboard</a></li>
-					<li><a href="profil.php">Profil</a></li>
-					<li><a href="data-kategori.php">Data Kategori</a></li>
-					<li><a href="data-produk.php">Data Produk</a></li>
-					<li><a href="keluar.php">Keluar</a></li>
+					
 
 				</ul>
 		</div>
