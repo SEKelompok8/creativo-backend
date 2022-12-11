b3e4cf6d4c5276adc573fd35dc40fddf1c003c74
Delmian <108231713+SEKelompok8@users.noreply.github.com>
2022-12-11 19:20:18 +0700

    Update data-kategori.php

--- data-kategori.php
+++ data-kategori.php
@@ -17,23 +17,20 @@
 	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 </head>
 <body>
-	<!--header -->
+	
 	<header>
 		<div class="container">
 			<h1><a href="dashboard.php">Creativo</a></h1>
 				<ul>
 			
 					<li><a href="dashboard.php">Home</a></li>
-					<li><a href="profil.php">Identitas</a></li>
-					<li><a href="data-kategori.php">Kategori</a></li>
-					<li><a href="data-produk.php">Data Produk</a></li>
-					<li><a href="keluar.php">Logout</a></li>
+					
 
 				</ul>
 		</div>
 	</header>
 
-	<!-- content -->
+	
 	<div class="section">
 		<div class="container">
 			<h3>Kategori</h3>
@@ -81,10 +78,6 @@
 		</div>
 	</div>
 
-	<!-- foater -->
-	<footer>
-		<div class="container">
-			<small>Copyright &copy; 2022 - Creativo.</small>
-	</footer>
+	
 </body>
 </html>
